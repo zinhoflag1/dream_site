8e9fdeeb6a1b6a4a24399c007279f0a170eec1e7
zinhoflag1 <zinhoflag1@gmail.com>
2022-07-14 22:46:32 -0300

    14.07.2022 removid

--- index.php
+++ index.php
@@ -12,56 +12,29 @@
 
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://www.atlasestateagents.co.uk/css/tether.min.css">
+    <style type="text/css">
+    	body {		
+    		background-image: url('imagens/fdo.jpg');
+    	}
+
+    	.container {
+    		background-color: #fff;
+    	}
+
+    </style>
+
+<script src="http://www.atlasestateagents.co.uk/javascript/tether.min.js"></script>
 
   </head>
+
+  <!--<video src='imagens/video/sf2_m7_chora.mp4' muted loop autoplay></video>-->
   <body>
 
-    <div class="container-fluid">
+    <div class="container">
 	<div class="row">
 		<div class="col-md-12">
-			<nav class="navbar navbar-expand-lg navbar-light bg-light">
-				 
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="navbar-toggler-icon"></span>
-				</button> <a class="navbar-brand" href="#">Brand</a>
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="navbar-nav">
-						<li class="nav-item active">
-							 <a class="nav-link" href="#">Link <span class="sr-only">(current)</span></a>
-						</li>
-						<li class="nav-item">
-							 <a class="nav-link" href="#">Link</a>
-						</li>
-						<li class="nav-item dropdown">
-							 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
-							<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-								 <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
-								<div class="dropdown-divider">
-								</div> <a class="dropdown-item" href="#">Separated link</a>
-							</div>
-						</li>
-					</ul>
-					<form class="form-inline">
-						<input class="form-control mr-sm-2" type="text"> 
-						<button class="btn btn-primary my-2 my-sm-0" type="submit">
-							Search
-						</button>
-					</form>
-					<ul class="navbar-nav ml-md-auto">
-						<li class="nav-item active">
-							 <a class="nav-link" href="#">Link <span class="sr-only">(current)</span></a>
-						</li>
-						<li class="nav-item dropdown">
-							 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
-							<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-								 <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
-								<div class="dropdown-divider">
-								</div> <a class="dropdown-item" href="#">Separated link</a>
-							</div>
-						</li>
-					</ul>
-				</div>
-			</nav>
+			<?php include_once('template/nav.php') ?>
 			<div class="carousel slide" id="carousel-428110">
 				<ol class="carousel-indicators">
 					<li data-slide-to="0" data-target="#carousel-428110">
@@ -72,8 +45,8 @@
 					</li>
 				</ol>
 				<div class="carousel-inner">
-					<div class="carousel-item">
-						<img class="d-block w-100" alt="Carousel Bootstrap First" src="https://www.layoutit.com/img/sports-q-c-1600-500-1.jpg">
+					<div class="carousel-item active">
+						<img class="d-block w-100" alt="Carousel Bootstrap First" src="imagens/banner_1.png">
 						<div class="carousel-caption">
 							<h4>
 								First Thumbnail label
@@ -94,7 +67,7 @@
 							</p>
 						</div>
 					</div>
-					<div class="carousel-item active">
+					<div class="carousel-item">
 						<img class="d-block w-100" alt="Carousel Bootstrap Third" src="https://www.layoutit.com/img/sports-q-c-1600-500-3.jpg">
 						<div class="carousel-caption">
 							<h4>
@@ -112,7 +85,18 @@
 	<div class="row">
 		<div class="col-md-4">
 			<h2>
-				Heading
+				Sistemas Multijogos
+			</h2>
+			<p>
+				Aqui você encontra alguns dos melhores sistemas multijogos da atualidade, todos já configurados e pronto pra começar a <a href='#'>"muelança"</a> aqui é sua área aquela que você conhece bem... area dos "guela" 
+			</p>
+			<p>
+				<a class="btn" href="paginas/sistemas.php">Veja mais aqui... »</a>
+			</p>
+		</div>
+		<div class="col-md-4">
+			<h2>
+				Listagem
 			</h2>
 			<p>
 				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
@@ -123,18 +107,7 @@
 		</div>
 		<div class="col-md-4">
 			<h2>
-				Heading
-			</h2>
-			<p>
-				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-			</p>
-			<p>
-				<a class="btn" href="#">View details »</a>
-			</p>
-		</div>
-		<div class="col-md-4">
-			<h2>
-				Heading
+				Diversão
 			</h2>
 			<p>
 				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
@@ -144,7 +117,10 @@
 			</p>
 		</div>
 	</div>
+
+	
 </div>
+<?php include_once('template/footer.php') ?>
 
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
---
+++ paginas/sistemas.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Bootstrap 4, from LayoutIt!</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://www.atlasestateagents.co.uk/css/tether.min.css">
+    <style type="text/css">
+    	
+    	body {
+
+    		background-image: url('../imagens/fdo.jpg');
+    		opacity:0.5	;
+    	}
+
+    	.container {
+
+    		/*background-color: #fff;*/
+    		
+    	}
+
+    </style>
+
+<script src="http://www.atlasestateagents.co.uk/javascript/tether.min.js"></script>
+
+  </head>
+  <body>
+
+    <div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<?php include_once('../template/nav.php'); ?>
+			
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-4">
+			<h2>
+				Batocera
+			</h2>
+			<p class="text-center"><img width="200" src="../imagens/batocera.png"></p>
+			<br>
+			<br>
+			<p class="text-justify">
+				Sistema open-source "código aberto", o batocera é um sistema baseado em Linux ou seja <a href='#'>chora !</a>, o bicho é bruto... Atualmente nossa versão está com quase 11 mil jogos !
+				confira a <a href='#'>lista de possíveis jogos</a>. Acesse o site oficial <a href=''>Batocera.org</a>.
+			</p>
+			<p class="">
+			<a href="#" class="btn btn-primary">Lista de Jogos</a></p>
+			
+		</div>
+		<div class="col-md-4">
+			<h2>
+				Recalbox
+			</h2>
+			<p class="text-center"><img width="170" src="../imagens/recalbox.svg"></p>
+			<br>
+			<br>
+			<p class="text-justify">
+				Sistema open-source "código aberto", o batocera é um sistema baseado em Linux ou seja <a href='#'>chora !</a>, o bicho é bruto... Atualmente nossa versão está com quase 11 mil jogos !
+				confira a <a href='#'>lista de possíveis jogos</a>. Acesse o site oficial <a href=''>Batocera.org</a>.
+			</p>
+			<p class="">
+			<a href="#" class="btn btn-primary">Lista de Jogos</a></p>
+		</div>
+		<div class="col-md-4">
+			<h2>
+				Retrorangepi
+			</h2>
+			<p class="text-center"><img width="350" src="../imagens/retrorangepie.png"></p>
+			<br>
+			<br>
+			<br>
+			<p class="text-justify">
+				Sistema open-source "código aberto", o batocera é um sistema baseado em Linux ou seja <a href='#'>chora !</a>, o bicho é bruto... Atualmente nossa versão está com quase 11 mil jogos !
+				confira a <a href='#'>lista de possíveis jogos</a>. Acesse o site oficial <a href=''>Batocera.org</a>.
+			</p>
+			<p class="">
+			<a href="#" class="btn btn-primary">Lista de Jogos</a></p>
+		</div>
+	</div>
+
+	
+
+</div>
+<?php include_once('../template/footer.php') ?>
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/scripts.js"></script>
+  </body>
+</html>
